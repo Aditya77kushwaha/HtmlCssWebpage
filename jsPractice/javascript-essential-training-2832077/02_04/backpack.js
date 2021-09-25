fab075a4761c7c4f91416553abf7fd423377a4f4
aditya77kushwaha <adityakushwaha2912@gmail.com>
2021-09-25 22:33:27 +0530

    Host on github

---
+++ jsPractice/javascript-essential-training-2832077/02_04/backpack.js
@@ -0,0 +1,26 @@
+const updateBackpack = (update) => {
+  console.info(update);
+};
+
+const backpack = {
+  name: "Everyday Backpack",
+  volume: 30,
+  color: "grey",
+  pocketNum: 15,
+  strapLength: {
+    left: 26,
+    right: 26,
+  },
+  lidOpen: false,
+  toggleLid: function (lidStatus) {
+    this.lidOpen = lidStatus;
+    updateBackpack(`Lid status changed.`);
+  },
+  newStrapLength: function (lengthLeft, lengthRight) {
+    this.strapLength.left = lengthLeft;
+    this.strapLength.right = lengthRight;
+    updateBackpack(`Strap lengths updated.`);
+  },
+};
+
+export default backpack;
